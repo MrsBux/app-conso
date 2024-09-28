b14ef424e44797a930cf9a003f0c22c77572e036
Camille SIESSE CUSCUSA <camille.siessecuscusa@gmail.com>
2024-09-28 15:57:35 +0200

    card partners

--- src/components/NosPartenaires/cardsPartners.jsx
+++ src/components/NosPartenaires/cardsPartners.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Form } from "react-bootstrap";
 import "../../style/css/cardpartner.css";
 import ModalT from "../ModalT";
 import BtnSupprimer from "../BtnSupprimer";
+import BtnModifier from "../BtnModifier";
 
 function CardPartner({
   id,
@@ -15,17 +17,28 @@ function CardPartner({
 }) {
   const navigate = useNavigate();
   const [isCardFlipped, setIsCardFlipped] = useState(false);
-  const handleAdmiBtn = () => {
-    console.log("click");
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({
+    name,
+    contactUrl,
+    type,
+    localisation,
+    description,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
-  const idP = id;
-
-  const handleDelete = (id) => {
+  const handleDelete = () => {
     const token = localStorage.getItem("token");
 
     fetch(
-      `https://domconso-d13067f1e717.herokuapp.com/api/partners/Delete/${idP}`,
+      `https://domconso-d13067f1e717.herokuapp.com/api/partners/Delete/${id}`,
       {
         method: "DELETE",
         headers: {
@@ -50,6 +63,46 @@ function CardPartner({
       });
   };
 
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const token = localStorage.getItem("token");
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    fetch(
+      `https://domconso-d13067f1e717.herokuapp.com/api/partners/Put/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          return response.text().then((text) => {
+            throw new Error(
+              `Erreur lors de la mise à jour du partenaire: ${text}`
+            );
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        alert("Partenaire mis à jour avec succès");
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error(
+          "Erreur détaillée lors de la mise à jour du partenaire:",
+          error
+        );
+        setError(error.message);
+      });
+  };
+
   return (
     <>
       <div
@@ -61,8 +114,7 @@ function CardPartner({
           <ModalT
             btnShow={
               <div className="logo__front">
-                <p className="logop__name"> {name}</p>
-                {/* <img src={logoPUrl} className="logo__front__img"></img> */}
+                <p className="logop__name">{name}</p>
               </div>
             }
             title={name}
@@ -72,7 +124,7 @@ function CardPartner({
                 <p>{localisation}</p>
                 <p>{type}</p>
                 <p>{description}</p>
-                <BtnSupprimer onClick={handleAdmiBtn} />
+                <BtnSupprimer onClick={handleDelete} />
               </div>
             }
             btnname={"Retour"}
@@ -90,9 +142,103 @@ function CardPartner({
                 <p>{type}</p>
                 <p>{description}</p>
                 <p>{localisation}</p>
-
                 <a href={contactUrl}>Site internet </a>
                 <BtnSupprimer onClick={handleDelete} />
+                <ModalT
+                  title="Modifier le partenaire"
+                  modalContent={
+                    <div className="modalt">
+                      <Form
+                        onSubmit={handleUpdate}
+                        className="form__ajout__partenaire form__ajout"
+                      >
+                        <Form.Group className="form__groupe" controlId="name">
+                          <Form.Label>Nom</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="name"
+                            placeholder="Nom du partenaire"
+                            value={formData.name}
+                            onChange={handleInputChange}
+                          />
+                        </Form.Group>
+
+                        <Form.Group
+                          className="form__groupe"
+                          controlId="description"
+                        >
+                          <Form.Label>Description</Form.Label>
+                          <Form.Control
+                            as="textarea"
+                            name="description"
+                            placeholder="Description"
+                            value={formData.description}
+                            onChange={handleInputChange}
+                          />
+                        </Form.Group>
+
+                        <Form.Group className="form__groupe" controlId="type">
+                          <Form.Label>Catégorie</Form.Label>
+                          <Form.Control
+                            as="select"
+                            name="type"
+                            value={formData.type}
+                            onChange={handleInputChange}
+                          >
+                            <option>Restaurateur</option>
+                            <option>Caviste</option>
+                            <option>Importateur</option>
+                            <option>Autre</option>
+                          </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group
+                          className="form__groupe"
+                          controlId="localisation"
+                        >
+                          <Form.Label>Localisation</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="localisation"
+                            placeholder="Localisation du partenaire"
+                            value={formData.localisation}
+                            onChange={handleInputChange}
+                          />
+                        </Form.Group>
+
+                        <Form.Group
+                          className="form__groupe"
+                          controlId="contactUrl"
+                        >
+                          <Form.Label>URL de contact</Form.Label>
+                          <Form.Control
+                            type="url"
+                            name="contactUrl"
+                            placeholder="https://example.com"
+                            value={formData.contactUrl}
+                            onChange={handleInputChange}
+                          />
+                        </Form.Group>
+
+                        <Form.Group className="form__groupe" controlId="logo">
+                          <Form.Label>Logo</Form.Label>
+                          <Form.Control
+                            type="file"
+                            name="logo"
+                            accept="image/*"
+                          />
+                        </Form.Group>
+
+                        <button className="btn__submit" type="submit">
+                          Mettre à jour
+                        </button>
+                      </Form>
+                      {error && <p className="error-message">{error}</p>}
+                    </div>
+                  }
+                  btnShow={<BtnModifier />}
+                  btnname={"Retour"}
+                />
               </div>
             }
             btnname={"Retour"}
