39e08c90a527c543a13fe1194dfef7f9cfb9a228
Camille SIESSE CUSCUSA <camille.siessecuscusa@gmail.com>
2024-10-01 11:40:17 +0200

    date

--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 // push
 const OrderList = ({ orders }) => {
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day} ${month} ${year}`;
+  }
+
   return (
     <div className="order-list">
       {orders.map((order) => (
@@ -16,6 +24,7 @@ const OrderList = ({ orders }) => {
           <p>Adresse: {order.adresse}</p>
           <p>Total: {order.totalPrice}€</p>
           <p>Type de paiement: {order.paiementType}</p>
+          <p>Date : {formatDate(order.Date)}</p>
           <p>Type de livraison: {order.deliveryType}</p>
           <h6>Vins commandés:</h6>
           <ul>
